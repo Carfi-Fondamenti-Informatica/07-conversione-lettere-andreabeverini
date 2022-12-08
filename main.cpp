fd9a15986ce52c122b5b71fb0a2f13ad877ac750
andreabeverini <117717829+andreabeverini@users.noreply.github.com>
2022-12-08 18:55:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+
+int main() {
+    char l=0;
+    cin>>l;
+    if (conversione(l)==true){
+        cout<<l<<endl;
+    }
+    else{
+        cout<<"errore"<<endl;
+    }
+    return 0;
 }
