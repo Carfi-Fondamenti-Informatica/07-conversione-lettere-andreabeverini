4330ffe6f4f067b70b86ac6df572f5e41f2726e6
andreabeverini <117717829+andreabeverini@users.noreply.github.com>
2022-12-08 18:52:05 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include "lib.h"
+using namespace std;
 
+bool conversione(char &a){
+    if (a>= 65 && a<=90){
+        a+=32;
+        return true;
+    }
+    else if (a>=97 && a<=122) {
+        a-=32;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
